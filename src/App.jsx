65ef6d0cfb6a9e7cd366be37a5c992f39e91f03f
ZuriTracks <zuritaivan333mdr@gmail.com>
2025-06-24 18:05:32 -0400

    mi primer comit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import Form from './components/Form';
+import List from './components/List';
+import './App.css';
+
+function App() {
+  const [items, setItems] = useState([]);
